a14a8fc2023132fbaec7566b40d0e14c06c303bb
Reza Alemy <ralemy@impinj.com>
2016-03-22 07:45:03 -0700

    debug on filter-jobs

--- nodes/filter-jobs.js
+++ nodes/filter-jobs.js
@@ -5,6 +5,7 @@
 module.exports = function (RED) {
     "use strict";
     var lib = require("./itemsense");
+
     function RunningJobNode(config) {
         RED.nodes.createNode(this, config);
         var node = this,
@@ -15,19 +16,23 @@ module.exports = function (RED) {
             node.status({fill: "yellow", shape: "ring", text: "getting running jobs"});
             itemSense.jobs.getAll().then(function (jobs) {
                 node.status({});
-                msg.topic="Jobs";
-                msg.payload = config.jobStatus==="ANY" ? jobs : _.filter(jobs,function(job){
+                msg.topic = "Jobs";
+                msg.payload = config.jobStatus === "ANY" ? jobs : _.filter(jobs, function (job) {
                     return job.status === config.jobStatus;
                 });
-                node.send([msg, {topic: "success", payload: "Extracted " + msg.payload.length +" jobs with status "+config.jobStatus}]);
-            }, function (err) {
-                console.log("general error getting jobs of status "+config.jobStatus, err);
                 node.send([msg, {
+                    topic: "success",
+                    payload: "Extracted " + msg.payload.length + " jobs with status " + config.jobStatus
+                }]);
+            }, function (err) {
+                console.log("general error getting jobs of status " + config.jobStatus, err);
+                node.send([null, {
                     topic: "failure",
-                    payload: lib.triageError(err,"Failed to get jobs with status "+config.jobStatus+". ")
+                    error: err,
+                    payload: lib.triageError(err, "Failed to get jobs with status " + config.jobStatus + ". ")
                 }]);
             }).catch(function (err) {
-                console.log("general error get jobs with status "+config.jobStatus, err,msg,config);
+                console.log("general error get jobs with status " + config.jobStatus, err, msg, config);
                 node.error(err, {payload: err});
             });
         });
