5da755d674fddf819be5f8e224701dc8df31de53
Reza Alemy <ralemy@impinj.com>
2016-06-23 07:03:44 -0700

    added fluent logger node

---
+++ nodes/76-log-to-fluent.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ralemy on 2/21/16.
+ * Node-Red node to stop an Itemsense Job
+ */
+module.exports = function (RED) {
+    "use strict";
+    var logger = require("fluent-logger");
+
+    function LogToFluent(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        logger.configure(config.tag,{
+            host:config.host,
+            port:config.port
+        });
+
+        this.on("input", function (msg) {
+            logger.emit(msg.topic,msg.payload)
+        });
+    }
+
+    RED.nodes.registerType("log-to-fluent", LogToFluent);
+};
