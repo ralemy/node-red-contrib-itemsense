bdaed45217d798b8dbf6ee2ebf7bb242b45ec50a
Reza Alemy <ralemy@impinj.com>
2016-06-24 20:24:34 -0700

    updated xArray reader definition sample

--- samples/lib/functions/itemsense/readerDef-xArray.js
+++ samples/lib/functions/itemsense/readerDef-xArray.js
@@ -2,24 +2,26 @@
 // outputs: 1
 //Sample Reader Definition Object for an xArray
 //fill in the blanks
-msg.payload = {
-    "name": "<...>",
-    "address": "xarray-<XX-XX-XX>.impinj.com",
-    "type": "XARRAY",
-    "placement": {
-        "x": -20.62,
-        "y": -7.67,
-        "z": 1.5,
-        "yaw": 180,
-        "pitch": 0,
-        "roll": 0,
-        "floor": "<...>"
-    },
-    "facility": "DEFAULT",
+function move_the_body_to_a_function_node(){
+    msg.payload = {
+        "name": "<...>",
+        "address": "xarray-<XX-XX-XX>.impinj.com",
+        "type": "XARRAY",
+        "placement": {
+            "x": -20.62,
+            "y": -7.67,
+            "z": 1.5,
+            "yaw": 180,
+            "pitch": 0,
+            "roll": 0,
+            "floor": "<...>"
+        },
+        "facility": "DEFAULT",
 //    "labels": null,
 //    "antennaZones": null,
 //    "connectionType": "LLRP",
-    "readerZone": "<...>"
-};
+        "readerZone": "<...>"
+    };
 
-return msg;
+    return msg;
+}
