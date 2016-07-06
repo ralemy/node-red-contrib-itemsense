1c7c77001fe17ef96eb7bc7da656a11d7d224893
Reza Alemy <ralemy@impinj.com>
2016-07-05 21:06:08 -0700

    fixed bug on static function call at get items history

--- nodes/40-item-history.js
+++ nodes/40-item-history.js
@@ -11,7 +11,7 @@ module.exports = function (RED) {
     function ItemHistoryNode(config) {
         RED.nodes.createNode(this, config);
         const node = this,
-            tagRetriever = lib.tagRetriever("items");
+            tagRetriever = lib.tagRetriever("history");
 
         this.on("input", function (msg) {
             if (msg.topic === "TerminateLoop")
